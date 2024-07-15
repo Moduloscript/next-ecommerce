571423ae000606281087e3fc5006044b1aa8ece8
moduloscript <maduemeka254@gmail.com>
2024-07-15 07:15:28 +0100

    added filter, productImages, ProductList, categoryList, slider  components

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -10,29 +10,78 @@ const CartModal = () => {
       {!cartItems ? (
         <p className='text-center'>Your cart is empty</p>
       ) : (
-        <>
-          <div className='text-center flex gap-4 '>
-            <Image
-              alt=''
-              src='/beads.jpg'
-              width={72}
-              height={96}
-              className='object-cover rounded-md'
-            />
-            <div className='flex flex-col justify-between w-full'>
-              {/* TOP  */}
-              <div className=''>
-                {/* TITLE */}
-                <h3>Product Name</h3>
-                <div className=''>$49</div>
-              </div>
-              {/* DESC */}
-              <div className=''>available</div>
-                {/* BOTTOM */}
-                <div className="">
-
-
+          <>
+            <h2 className="text-xl">Shopping Cart</h2>
+          {/* // LIST */}
+          <div className='flex flex-col gap-8'>
+            {/* ITEM */}
+            <div className='flex gap-4 '>
+              <Image
+                alt=''
+                src='/beads.jpg'
+                width={72}
+                height={96}
+                className='object-cover rounded-md'
+              />
+              <div className='flex flex-col justify-between w-full'>
+                {/* TOP  */}
+                <div className=''>
+                  {/* TITLE */}
+                  <div className='flex items-center justify-between gap-8'>
+                    <h3 className='font-semibold'>Product Name</h3>
+                    <div className='p-1 bg-gray-50 rounded-sm'>$49</div>
+                  </div>
                 </div>
+                {/* DESC */}
+                <div className='text-sm text-gray-500'>available</div>
+                {/* BOTTOM */}
+                <div className=' flex justify-between text-sm'>
+                  <span className='text-gray-500'>Qty. 2</span>
+                  <span className='text-blue-500'>Remove</span>
+                </div>
+              </div>
+            </div>
+
+            {/* ITEM */}
+            <div className='flex gap-4 '>
+              <Image
+                alt=''
+                src='/beads.jpg'
+                width={72}
+                height={96}
+                className='object-cover rounded-md'
+              />
+              <div className='flex flex-col justify-between w-full'>
+                {/* TOP  */}
+                <div className=''>
+                  {/* TITLE */}
+                  <div className='flex items-center justify-between gap-8'>
+                    <h3 className='font-semibold'>Product Name</h3>
+                    <div className='p-1 bg-gray-50 rounded-sm'>$49</div>
+                  </div>
+                </div>
+                {/* DESC */}
+                <div className='text-sm text-gray-500'>available</div>
+                {/* BOTTOM */}
+                <div className=' flex justify-between text-sm'>
+                  <span className='text-gray-500'>Qty. 2</span>
+                  <span className='text-blue-500'>Remove</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* // BOTTOM */}
+          <div className=''>
+            <div className='flex items-center justify-between font-semibold'>
+              <span className=''>SubTotal</span>
+              <span className=''>$49</span>
+            </div>
+            <p className='text-gray-500 text-sm mt-2 mb-4'>
+              Shipping and taxes calculated at Checkout
+            </p>
+            <div className='flex justify-between text-sm'>
+              <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">View Cart</button>
+              <button className="rounded-md py-3 px-4 bg-black text-white">CheckOut</button>
             </div>
           </div>
         </>
